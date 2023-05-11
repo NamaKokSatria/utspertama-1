e79337c63660230fe9b457b0b03ec59b2c02cef0
NamaKokSatria <rizaldwis15@gmail.com>
2023-05-11 16:45:12 +0700

    uts

--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,30 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
-        // MEnambah KOmentar
-    }
-}
+public class Mhs extends User {
+    public class Mhs extends User {
+        private String nama;
+        private String nim;
+        private int smt;
+    
+        public Mhs (String username, String password) {
+          super("ini_username", "ini_password");  
+        }    
+        public void setNama(String nama) {
+            this.nama = nama;
+        }
+        public String getNama() {
+            return this.nama = nama;
+        }
+        public void setNim(String nim) {
+            this.nim = nim;
+        }
+        public String getNim() {
+            return this.nim = nim;
+        }
+        public void setSmt(int smt) {
+            this.smt = smt;
+        }
+        public int getSmt() {
+            return this.smt = smt;
+        }
+}    
