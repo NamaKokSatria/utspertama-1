d43bad30115796ad3e37286cf52bce2f9f573449
NamaKokSatria <rizaldwis15@gmail.com>
2023-05-11 16:41:40 +0700

    public class

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import pengguna.*;
 class Main {
     public static void main(String[] args) {
 
+        Mhs mhs = new Mhs("ini_username", "ini_password", "Mochhamad Idris", "A123", 3);
+        
+        System.out.println(mhs.getNama()); 
+        System.out.println(mhs.getNim()); 
+        System.out.println(mhs.getSmt());
+        System.out.println("Username = ini username");
+        System.out.println("Password = ini password");
+        
+        }
     }
-}
