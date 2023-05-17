f09bf5fe297f27990448ce3e87ed6dfa617abd8e
NamaKokSatria <rizaldwis15@gmail.com>
2023-05-17 16:08:54 +0700

    uts perbaikan

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-import pengguna.*;
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
 
-        Mhs mhs = new Mhs("ini_username", "ini_password", "Mochhamad Idris", "A123", 3);
-        
-        System.out.println(mhs.getNama()); 
-        System.out.println(mhs.getNim()); 
-        System.out.println(mhs.getSmt());
-        System.out.println("Username = ini username");
-        System.out.println("Password = ini password");
-        
+            Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Nama Kok Satria", "G.111.19.0009", 8);
+    
+            // mhs.setNama("Nama Kok Satria");
+            // mhs.setNim("G.111.19.0009");
+            // mhs.setSmt(8);
+    
+            System.out.println("Nama Mhs : " + mhs.getNama());
+            System.out.println("Nim Mhs : " + mhs.getNim());
+            System.out.println("Smt Mhs : " + mhs.getSmt());
+            System.out.println("Username : " + mhs.getUsername());
+            System.out.println("Password : " + mhs.getPassword());
+    
         }
     }
